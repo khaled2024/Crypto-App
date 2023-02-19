b85b0d3973e8d4fdc105ac41e615140f204c0599
khaled hussien <57367756+khaled2024@users.noreply.github.com>
2023-02-20 00:06:25 +0200

    Initial Commit

---
+++ Crypto App/Crypto_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Crypto_AppApp.swift
+//  Crypto App
+//
+//  Created by KhaleD HuSsien on 20/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct Crypto_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
