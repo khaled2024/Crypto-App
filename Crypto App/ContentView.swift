65c8430c0ce86990e7fb18bccba2d0ee5944c1f5
khaled hussien <57367756+khaled2024@users.noreply.github.com>
2023-02-21 11:23:40 +0200

    Setting up Xcode Project and a custom color theme

--- Crypto App/ContentView.swift
+++ Crypto App/ContentView.swift
@@ -13,7 +13,8 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("Hello, World!")
+                .foregroundColor(Color.theme.accent)
         }
         .padding()
     }
---
+++ Crypto App/Extensions/Color.swift
@@ -0,0 +1,26 @@
+//
+//  Color.swift
+//  Crypto App
+//
+//  Created by KhaleD HuSsien on 21/02/2023.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color{
+    static let theme = ColorTheme()
+    static let launch = LaunchTheme()
+}
+struct ColorTheme {
+    let accent = Color("AccentColor")
+    let background = Color("BackgroundColor")
+    let green = Color("GreenColor")
+    let red = Color("RedColor")
+    let secondaryText = Color("SecondaryTextColor")
+}
+struct LaunchTheme {
+    let accent = Color("LaunchAccentColor")
+    let background = Color("LaunchBackgroundColor")
+    
+}
